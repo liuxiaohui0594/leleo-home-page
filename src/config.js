ee474c59e324e91c77b7d859135fb479d2d56698
liuxiaohui0594 <878576308@qq.com>
2026-04-08 23:34:56 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -161,7 +161,7 @@ const config = {
 		{ go: "🔍 前往", img: "/img/1/fengjing3.jpg", title: "我的VPN", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://liuxiaohui.dpdns.org/Liuxiaohui_520", show: false },
 		{ go: "🔍 前往", img: "/img/1/fengjing10.jpg", title: "百度网盘", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://pan.baidu.com/disk/main?_at_=1775644084687#/index?category=all", show: false },
 		{ go: "🔍 前往", img: "/img/1/fengjing9.jpg", title: "虎牙资源", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://www.huyaapi.com/", show: false },
-		{ go: "🔍 前往", img: "/img/1/fengjing7.jpg", title: "其他", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "suiji.lz520.eu.cc", show: false },
+		{ go: "🔍 前往", img: "/img/1/fengjing7.jpg", title: "电视直播源", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "ds.lz520.eu.cc", show: false },
 		
 	]
 	
